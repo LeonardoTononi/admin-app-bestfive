5e66a1979836f0b6232cfe464d17861a961b1855
Leonardo Tononi <easaaa@protonmail.com>
2019-11-04 22:47:52 +0100

    Created structure project

---
+++ src/pages/Add-place/add-place.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './add-place.styles.scss'
+
+const AddPlace = () => {
+
+  return (
+    <div className="container">
+      <h1>Add places form</h1>
+    </div>
+  )
+}
+
+export default AddPlace
